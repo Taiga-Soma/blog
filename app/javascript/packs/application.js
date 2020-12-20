18edba6a7276c5ff59ee122c63335fbfac6f5d17
Taiga-Soma <allvictory111@gmail.com>
2020-12-20 18:28:40 +0900

    commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require("@rails/ujs").start()
 // require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-// require.context('../images', true)
+require("./tag")
 
 //= require jquery3
 //= require popper
